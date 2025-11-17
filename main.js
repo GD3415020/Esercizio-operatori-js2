ee80e450e12b977de733c09fa97328006766425c
GDIsanto <logantom85@gmail.com>
2025-11-17 11:28:34 +0100

    completamento esercizio

---
+++ main.js
@@ -0,0 +1,8 @@
+let anno = prompt('Dimmi in che anno siamo');
+let nascita = prompt('Inserisci il tuo anno di nascita');
+
+let eta = anno - nascita;
+
+let differenza = 100 - eta;
+
+console.log('Hai ' + eta + ' anni e te ne mancano ' + differenza + ' per compiere 100 anni');
